dafad83a4c2d6af2375b963de7118ddd774f6c61
Sanat Kulkarni <87092449+SanatKulkarni@users.noreply.github.com>
2023-11-19 10:03:27 +0530

    Add files via upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,42 @@
-import { React, useState} from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HomePage from '../components/HomePage';
 import AddUser from '../components/AddUser';
-import ManufactureMedicine from "../components/ManufactureMedicine";
-import SellMedicine from "../components/SellMedicine";
-
+import ManufactureMedicine from '../components/ManufactureMedicine';
+import SellMedicine from '../components/SellMedicine';
 
 const App = () => {
   return (
-    <div>
-      <AddUser />
-    </div>
-  )
-}
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/Home">Home</Link>
+            </li>
+            <li>
+              <Link to="/add-user">Add User</Link>
+            </li>
+            <li>
+              <Link to="/manufacture-medicine">Manufacture Medicine</Link>
+            </li>
+            <li>
+              <Link to="/sell-medicine">Sell Medicine</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/Home" element={<HomePage />} />
+          <Route path="/Add-User" element={<AddUser />} />
+          <Route path="/Manufacture-Medicine" element={<ManufactureMedicine />} />
+          <Route path="/Sell-Medicine" element={<SellMedicine />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+
 
 export default App;
